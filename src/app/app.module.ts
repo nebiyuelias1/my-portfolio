a3b897361fafa4232754d3810dcbf4e028db5c2d
eliasn <nebiyuelias1@gmail.com>
2021-08-20 22:41:54 +0300

    Add hero.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import { HeroComponent } from './hero/hero.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeroComponent,
+    SocialLinksComponent
   ],
   imports: [
     BrowserModule,
